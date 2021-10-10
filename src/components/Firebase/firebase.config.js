2e8e4324e7bae8fde48cbe83df5812340e8718c3
Arifulislamt <arifulislamt06@gmail.com>
2021-10-10 07:25:05 +0600

    fast commit

---
+++ src/components/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAro1_XQW-ZWOwazzEbGlXD9yyrZfmHtzo",
+    authDomain: "ema-john-simple-9350d.firebaseapp.com",
+    projectId: "ema-john-simple-9350d",
+    storageBucket: "ema-john-simple-9350d.appspot.com",
+    messagingSenderId: "72614217783",
+    appId: "1:72614217783:web:8aeb9f2431fc5f5eab17b2"
+  };
+  export default firebaseConfig;
